b07e5a8df7d7a6ec44b97b4ec9b4f9d0b314ef6b
Maarten de Jager <maartendejager@gmail.com>
2018-12-06 17:43:39 +0000

    working on dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -14,12 +14,16 @@
             </div>
         </div>
     </div>
-    <div class="row">
+    <div class="row mt-3">
     	{{-- main Scan stuff --}}
     	<div class="col-md-9">
     		<div class="row mb-5">
-    			<div class="col">
-		    		<h5>Mijn Scans</h5>
+    			<div class="col border">
+    				<div class="row">
+    					<div class="col-12 pt-2" >
+				    		<h6>Mijn Scans</h6>
+    					</div>
+    				</div>
 		    		<div class="row p-1 bg-white text-secondary border">
 		    			<div class="col-12">
 		    				<i class="material-icons md-inactive"> star </i> hier is een scan
@@ -39,8 +43,12 @@
     			</div>
     		</div>
     		<div class="row mb-5">
-    			<div class="col">
-		    		<h5>Mijn groepen</h5>
+    			<div class="col shadow">
+    				<div class="row">
+    					<div class="col-12 pt-2 border-top border-primary">
+				    		<h6>Mijn groepen</h6>
+    					</div>
+    				</div>
 		    		<div class="row p-1 bg-white text-secondary border">
 		    			<div class="col-12">
 		    				hier is een groep
@@ -65,20 +73,20 @@
     	{{-- Sidebar functions --}}
     	<div class="col-md-3">
     		<a href=" {{ route('introductiefilm') }} " title="">
-    		    <div class="card card__dashboard mt-4">
+    		    <div class="card card__dashboard mb-4">
     		        <div class="card-body">
 		    		    <h5 class="card-title">Instructiefilm</h5>
     		            <img src="/img/films.jpg" alt="">
     		        </div>
     		    </div>
     		</a>
-            <div class="card card__dashboard mt-4" data-toggle="tooltip" data-placement="top" title="De kennisbank is nog in ontwikkeling">
+            <div class="card card__dashboard mb-4" data-toggle="tooltip" data-placement="top" title="De kennisbank is nog in ontwikkeling">
                 <div class="card-body">
                     <h5 class="card-title disabled">Kennisbank</h5>
                     <img src="/img/book_disabled.jpg" alt="">
                 </div>
             </div>
-            <div class="card card__dashboard mt-4" data-toggle="tooltip" data-placement="top" title="De kennisbank is nog in ontwikkeling">
+            <div class="card card__dashboard mb-4" data-toggle="tooltip" data-placement="top" title="De kennisbank is nog in ontwikkeling">
                 <div class="card-body">
                     <h5 class="card-title disabled">Scans vergelijken</h5>
                     <img src="/img/book_disabled.jpg" alt="">
