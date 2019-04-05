e6026efb7606e4b5cb6cfc6dc31382478c8dc8cb
Maarten de Jager <maartendejager@gmail.com>
2019-04-05 11:49:25 +0200

    runtrough upload

---
+++ resources/views/partials/videos.blade.php
@@ -0,0 +1,38 @@
+<!-- Modal -->
+    <div class="modal fade" id="filmsModal" tabindex="-1" role="dialog" aria-labelledby="filmsModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-xl" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="filmsModalLabel">Videos</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/YgmIibSnZs0?rel=0" allowfullscreen></iframe>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-4">
+                            <div class="embed-responsive embed-responsive-16by9">
+                                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/YgmIibSnZs0?rel=0" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="embed-responsive embed-responsive-16by9">
+                                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/YgmIibSnZs0?rel=0" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="embed-responsive embed-responsive-16by9">
+                                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/YgmIibSnZs0?rel=0" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Sluit</button>
+                </div>
+            </div>
+        </div>
+</div>
