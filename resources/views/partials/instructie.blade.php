5cc03cb8b2e6f1e98c499f7a420ec3367d38f3b7
Maarten de Jager <maartendejager@gmail.com>
2019-04-08 12:09:35 +0200

    some updates

--- resources/views/partials/instructie.blade.php
+++ resources/views/partials/instructie.blade.php
@@ -9,21 +9,9 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <p>Uit onderzoek blijkt dat schuld kan worden voorkomen als publieke en private partijen samenwerken en inzicht hebben in: </p>
-                    <ul>
-                        <li>Drempels voor vragen van hulp</li>
-                        <li>Communicatiestrategieën </li>
-                        <li>Effectieve samenwerking </li>
-                        <li>Professionaliteit van de uitvoering</li>
-                    </ul>
-                    <p>Deze gesprekshulp geeft jou en je geprekspartners inzicht in verbeterpunten ten aanzien van deze thema’s en geeft gerichte handvatten om schulden vroegtijdig te signaleren</p>
-                    <p><strong>Stap 1:</strong></p>
-                    <p>Bepaal met wie je het beste kunt samenwerken.</p>
-                    <p>De “selectiehulp gesprekspartners” geeft je inzicht in de partners uit jouw regio met wie je dit gesprek kunt voeren.</p>
-                    <p><strong>Stap 2:</strong></p>
-                    <p>Nodig jouw gesprekspartners uit en ontdek samen aan de hand van deze gesprekshulp hoe je schulden in jouw regio vroegtijdig kunt voorkomen.</p>
-                    <a href="/img/Tips voor het gesprek.jpg" target="_blank">
-                    <a href="/pdf/Handleiding digitale gesprekshulp.pdf" class="btn"> Lees de handleiding</a>
+                    <p>Hier komt een tekst over hoe de Participatie.scan werkt, en een link naar de handleiding.</p>
+                    
+                    <a href="#" class="btn"> Lees de handleiding</a>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Sluit</button>
