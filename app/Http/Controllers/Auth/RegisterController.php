0a30e55b0bc12946d496aaea55d46b85eeb98939
Maarten de Jager <maartendejager@gmail.com>
2019-04-10 10:21:05 +0200

    fixes unique phone number

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -63,7 +63,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'phone' => ['required', 'regex:/(06)[0-9]{8}/', 'unique:users'],
+            'phone_number' => ['required', 'regex:/(06)[0-9]{8}/', 'unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
         ]);
     }
@@ -79,7 +79,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'phone_number' => $data['phone'],
+            'phone_number' => $data['phone_number'],
             'password' => Hash::make($data['password']),
         ]);
     }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -48,14 +48,14 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="phone" class="col-sm-4 col-form-label text-md-right">{{ __('Telefoon nummer') }}</label>
+                            <label for="phone_number" class="col-sm-4 col-form-label text-md-right">{{ __('Telefoon nummer') }}</label>
 
                             <div class="col-md-6">
-                                <input id="phone" type="phone" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" name="phone" value="{{ old('phone') }}" required autofocus>
+                                <input id="phone_number" type="phone_number" class="form-control{{ $errors->has('phone_number') ? ' is-invalid' : '' }}" name="phone_number" value="{{ old('phone_number') }}" required autofocus>
 
-                                @if ($errors->has('phone'))
+                                @if ($errors->has('phone_number'))
                                     <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('phone') }}</strong>
+                                        <strong>{{ $errors->first('phone_number') }}</strong>
                                     </span>
                                 @endif
                             </div>
