8b587cdd38da6bba50c715fbf3a54bf729e55004
Maarten de Jager <maartendejager@gmail.com>
2019-04-10 11:07:04 +0200

    fixes typo give group a name on slingle scan

--- resources/views/createsinglescan/title.blade.php
+++ resources/views/createsinglescan/title.blade.php
@@ -5,9 +5,9 @@
 		<div class="row justify-content-center">
 			<div class="col-8">
 				<div class="card card--create">
-					<h5 class="card-header bg-primary text-white">Nieuwe groep: Naam</h5>
+					<h5 class="card-header bg-primary text-white">Nieuwe sessie: Naam</h5>
 					<div class="card-body">
-						<h5 class="card-title">Geef je groep een naam</h5>
+						<h5 class="card-title">Geef je sessie een naam</h5>
 						<form action="{{ route('createsinglescan.storetitle') }}" method="post" accept-charset="utf-8">
 						    {{ csrf_field() }}
 							@include('createsinglescan.partials.titleform', ['submittext' => 'sla naam op'])
