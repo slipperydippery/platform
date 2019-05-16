b0c860e7ea170e79469542151caa1d7985cf6f20
Maarten de Jager <maartendejager@gmail.com>
2019-05-16 21:56:17 +0200

    Refactored to API/ArticletypeController

--- app/Articletype.php
+++ app/Articletype.php
@@ -9,6 +9,17 @@ class Articletype extends Model
 {
     protected $guarded = [];
 
+    /**
+     *  Setup model event hooks
+     */
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $model->order = $model->count() + 1;
+        });
+    }
+
     public function articles()
     {
     	return $this->belongsToMany(Article::class);
--- routes/api.php
+++ routes/api.php
@@ -19,13 +19,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::apiResources([
 	'answer' 		=> 'Api\AnswerController',
-	'article' 		=> 'Api\ArticleController'
+	'article' 		=> 'Api\ArticleController',	
+	'articletype'	=> 'Api\ArticletypeController',
 ]);
 
 Route::post('/articlepostpatch', 'Api\ArticleController@postpatch');
 
-
-// Route::resource('article', 'ApiArticleController');
 Route::resource('theme', 'ApiThemeController');
 Route::resource('group', 'ApiGroupController');
 Route::resource('scan', 'ApiScanController');
@@ -34,7 +33,6 @@ Route::resource('followup', 'ApiFollowupController');
 Route::resource('district', 'ApiDistrictController');
 Route::resource('session', 'ApiSessionController');
 Route::resource('comparison', 'ApiComparisonController');
-Route::resource('articletype', 'ApiArticletypeController');
 
 Route::get('/scan/{scan}/question/{question}/getanswers', 'ApiScanQuestionController@getanswers');
 
