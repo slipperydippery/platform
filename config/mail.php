3e1cea45b8ef9a6cd389859cac4581cff48a4ac3
Maarten de Jager <maartendejager@gmail.com>
2019-09-23 15:30:47 +0200

    updates email to participatiescan.com

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'info@participatiescan.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'info@mg.participatiescan.com'),
         'name' => env('MAIL_FROM_NAME', 'Participatiescan'),
     ],
 
